7a6f6b9569d70cd71189a645548f1d02a22c56b9
Ryan <ryanjerickson@gmail.com>
2023-06-29 21:52:02 -0500

    React App Setup

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="title-header">
+        <h2>WELCOME TO THE CSC WX APP!</h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
