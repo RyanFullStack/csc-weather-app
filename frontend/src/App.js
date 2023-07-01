2eaef48e5fc6a0f4c0cac2da636f861de270ab9d
Ryan <ryanjerickson@gmail.com>
2023-06-30 21:20:21 -0500

    Test Complete

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
 import WindSpeed from './components/wind';
-import GustChart from './components/gusts';
 
 function App() {
   return (
     <>
       <div className="App">
-          <GustChart />
           <WindSpeed />
       </div>
     </>
