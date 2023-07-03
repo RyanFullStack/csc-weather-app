becced915fc13215ba97835b78fa13332a96b404
Ryan <ryanjerickson@gmail.com>
2023-07-03 02:30:23 -0500

    Spacing

--- frontend/src/components/wind.js
+++ frontend/src/components/wind.js
@@ -123,7 +123,7 @@ function Wind() {
         <div className="wind-component">
             <div className='wind-component-top'>
                 <div className="wind-speed">
-                    25 <div className="small">kts</div>
+                    {speed} <div className="small">kts</div>
                 </div>
                 <div className="wind-anamometer">
                     <img src={arrow} alt='Wind Direction' className={`arrow  ${trackDirection}`}></img>
