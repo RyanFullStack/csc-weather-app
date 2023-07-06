6c0a1cc4a72bbbaaabda92c63e6a9346095ec5b8
Ryan <ryanjerickson@gmail.com>
2023-07-06 00:10:27 -0500

    Alt Tag

--- frontend/src/components/hangar.js
+++ frontend/src/components/hangar.js
@@ -15,7 +15,7 @@ function HangarCam() {
 
   return (
     <div className='hangar-cam'>
-      <img src='http://webcam.skydivecsc.com/hangar_nw?1688604278611' id='cam' alt='HANGAR CAMERA FEED NOT FOUND, THIS IS A PROBLEM WITH THE SOURCE AND NOT THIS APP. VISIT SKYDIVECSC.COM/SKYDIVING-WEBCAMS TO CONFIRM.'/>
+      <img src='http://webcam.skydivecsc.com/hangar_nw?1688604278611' id='cam' alt='Hangar camera feed not found, This is a problem with the source and not this app. Visit skydivecsc.com/skydiving-webcams to confirm.'/>
     </div>
   );
 }
