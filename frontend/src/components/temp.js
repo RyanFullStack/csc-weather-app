4aee0b8a18b0f409b184c3737a72b7bcb214f8ba
Ryan <ryanjerickson@gmail.com>
2023-07-17 18:01:19 -0500

    Nav Bar Links

--- frontend/src/components/temp.js
+++ frontend/src/components/temp.js
@@ -1,5 +1,9 @@
 import { useContext, useEffect } from "react"
 import { WeatherContext } from "../context/WeatherContext"
+import fb from '../images/fb.png'
+import audio from '../images/audio.png'
+import ig from '../images/ig.png'
+import wa from '../images/wa.png'
 
 function CurrentTemp() {
     const { temp, tempC, tempSetting, setTempSetting } = useContext(WeatherContext)
@@ -25,8 +29,14 @@ function CurrentTemp() {
     }
 
     return (
-        <div className='temp-content' onClick={handleClick}>
-            {tempSetting === 'true' && temp ? `${temp}º F` : tempC ? `${tempC}º C` : ''}
+        <div className='temp-content'>
+            <div className="temp-logos">
+                <a href='https://www.facebook.com/groups/csc.experienced' target='_blank'><img src={fb} /></a>
+                <a href='https://www.instagram.com/skydivecsc/' target='_blank'><img src={ig} /></a>
+                <a href='https://chat.whatsapp.com/K4on8ni6xKd1nSta35CQAK' target='_blank'><img src={wa} /></a>
+                <a href='http://audio.skydivecsc.com/' target='_blank'><img src={audio} /></a>
+            </div>
+            <div className="temp-temp" onClick={handleClick}>{tempSetting === 'true' && temp ? `${temp}º F` : tempC ? `${tempC}º C` : ''}</div>
         </div>
     )
 }
