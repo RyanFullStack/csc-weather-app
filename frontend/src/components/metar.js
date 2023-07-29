3bea8fcc99942a07994d5cf223bb0f117a72677a
Ryan <ryanjerickson@gmail.com>
2023-07-28 23:12:52 -0500

    Added Footer Info

--- frontend/src/components/metar.js
+++ frontend/src/components/metar.js
@@ -1,11 +1,16 @@
 import { useContext } from "react";
 import { WeatherContext } from '../context/WeatherContext';
+import './metar.css';
 
 function Metar() {
     const { metar }  = useContext(WeatherContext)
 
     return (
-        metar
+        <div className="metar-container">
+            <p><a href='https://github.com/RyanFullStack' target="_blank" rel='noreferrer'>Created by: Ryan Erickson</a></p>
+            {metar}
+            <p><a href='https://github.com/RyanFullStack' target="_blank" rel='noreferrer'>Github</a></p>
+        </div>
     )
 }
 
