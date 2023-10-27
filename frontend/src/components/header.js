153b6ecf40e830f3d3a3b5d932fa2b6875d06457
Ryan <ryanjerickson@gmail.com>
2023-10-26 22:24:29 -0500

    spooky

--- frontend/src/components/header.js
+++ frontend/src/components/header.js
@@ -5,10 +5,14 @@ import CurrentTemp from './temp';
 import Theme from './theme';
 import LiveStatus from './livestatus';
 import audio from '../images/audio.png'
-import redLight from '../images/redlight.png'
-import yellowLight from '../images/yellowlight.png'
-import greenLight from '../images/greenlight.png'
-import noLight from '../images/nolight.png'
+// import redLight from '../images/redlight.png'
+import redLightSpooky from '../images/redlightspooky.png'
+// import yellowLight from '../images/yellowlight.png'
+import yellowLightSpooky from '../images/yellowlightspooky.png'
+// import greenLight from '../images/greenlight.png'
+import greenLightSpooky from '../images/greenlightspooky.png'
+// import noLight from '../images/nolight.png'
+import noLightSpooky from '../images/nolightspooky.png'
 
 function Header() {
 
@@ -23,9 +27,9 @@ function Header() {
                 <LiveStatus />
             </div>
             <div className='time-container'>
-                {jumpruns[0] ? <img src={jumpruns[0]?.beerLight ? yellowLight : jumpruns[0]?.weatherHold ? redLight : jumpruns[0]?.heading ? greenLight : null} alt='beerlight'/> : <img src={noLight} alt='beerlight'/>}
+                {jumpruns[0] ? <img src={jumpruns[0]?.beerLight ? yellowLightSpooky : jumpruns[0]?.weatherHold ? redLightSpooky : jumpruns[0]?.heading ? greenLightSpooky : null} alt='beerlight'/> : <img src={noLightSpooky} alt='beerlight'/>}
                 <GetCst />
-                {jumpruns[0] ? <img src={jumpruns[0]?.beerLight ? yellowLight : jumpruns[0]?.weatherHold ? redLight : jumpruns[0]?.heading ? greenLight : null} alt='beerlight'/> : <img src={noLight} alt='beerlight'/>}
+                {jumpruns[0] ? <img src={jumpruns[0]?.beerLight ? yellowLightSpooky : jumpruns[0]?.weatherHold ? redLightSpooky : jumpruns[0]?.heading ? greenLightSpooky : null} alt='beerlight'/> : <img src={noLightSpooky} alt='beerlight'/>}
             </div>
             {window.location.pathname !== '/loadingarea' ? <div className="temp-logos">
                 <a href='http://audio.skydivecsc.com/' target='_blank' rel='noreferrer'><img src={audio} alt='audio' /></a>
