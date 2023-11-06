700a55a1baca8d5160f3e9b09b78b78ada836a15
Ryan <ryanjerickson@gmail.com>
2023-11-05 19:14:03 -0600

    refactor time

--- frontend/src/components/Time/index.js
+++ frontend/src/components/Time/index.js
@@ -1,11 +1,9 @@
 import { useEffect, useState } from "react"
 
 function GetCst() {
-    const [currentTime, setCurrentTime] = useState('')
     const [currentDate, setCurrentDate] = useState('')
-    const [timeOfDay, setTimeOfDay] = useState('')
     const [currentDay, setCurrentDay] = useState('')
-
+    const [time, setTime] = useState('')
 
     function getNewDate() {
         return new Date()
@@ -16,16 +14,14 @@ function GetCst() {
             const timeObj = getNewDate().toLocaleString('en-US', { timeZone: 'America/Chicago' })
             const timeArr = timeObj.split(',')
             const [date, time] = timeArr
+            setTime(time)
 
-            setCurrentTime(time.split(' ')[1])
-            setTimeOfDay(time.split(' ')[2])
             const newDate = date.split('/')
             newDate.pop()
             const today = newDate.join('/')
             setCurrentDate(today)
 
             const getDay = getNewDate()
-
             setCurrentDay(getDay.toString().split(' ')[0])
 
         }, 1000)
@@ -38,8 +34,7 @@ function GetCst() {
 
     return (
         <div className="time-component">
-            <div>{currentDay} {currentDate} {currentTime} {timeOfDay}</div>
-            <div></div>
+            <div>{currentDay} {currentDate} {time}</div>
         </div>
     )
 }
