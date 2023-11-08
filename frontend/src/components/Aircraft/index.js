22479d2d326beddd67716317c353b3149a36cbbb
Ryan <ryanjerickson@gmail.com>
2023-11-08 12:37:26 -0600

    zoom level

--- frontend/src/components/Aircraft/index.js
+++ frontend/src/components/Aircraft/index.js
@@ -2,7 +2,7 @@ function AircraftTracker() {
 
     return (
         <div className="aircraft-tracker">
-           <embed src="https://globe.adsbexchange.com/?airport=KRPJ&hideSidebar&hideButtons&enableLabels&extendedLabels=1&zoom=11"/>
+           <embed src="https://globe.adsbexchange.com/?airport=KRPJ&hideSidebar&hideButtons&enableLabels&extendedLabels=1&zoom=10"/>
         </div>
     )
 }
