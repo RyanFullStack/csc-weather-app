0742585132fb16b2c1222beaf47eb8cc0ae119d0
Ryan <ryanjerickson@gmail.com>
2023-11-08 17:34:27 -0600

    iframe fix

--- frontend/src/components/Aircraft/index.js
+++ frontend/src/components/Aircraft/index.js
@@ -6,11 +6,16 @@ function AircraftTracker() {
 
     const url = "https://globe.adsbexchange.com/?airport=KRPJ&hideSidebar&hideButtons&enableLabels&extendedLabels=1&zoom=11"
 
+    const iframeStyle = {
+        width: "1000px", // forcing iframe to load desktop view
+        height: "800px", // Forcing iframe to load desktop view
+    };
+
     return (
         <div className="aircraft-tracker">
             <iframe title='aircraft-tracker' src={
                 darkTheme === 'true' ? `${url}&mapdim=.3` : `${url}&mapdim=.1`
-                } />
+                } style={iframeStyle}/>
         </div>
     )
 }
