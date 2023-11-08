00d1cc44adf17f90ce971dbb14a3fbd067660100
Ryan <ryanjerickson@gmail.com>
2023-11-08 12:51:19 -0600

    embed to iframe

--- frontend/src/components/Aircraft/index.js
+++ frontend/src/components/Aircraft/index.js
@@ -2,7 +2,7 @@ function AircraftTracker() {
 
     return (
         <div className="aircraft-tracker">
-           <embed src="https://globe.adsbexchange.com/?airport=KRPJ&hideSidebar&hideButtons&enableLabels&extendedLabels=1&mapDim=.1&zoom=10"/>
+           <iframe title='aircraft-tracker' src="https://globe.adsbexchange.com/?airport=KRPJ&hideSidebar&hideButtons&enableLabels&extendedLabels=1&mapDim=.1&zoom=11"/>
         </div>
     )
 }
