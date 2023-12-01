418c210ae7011e45b2a1c680344b973e4e593788
Ryan <ryanjerickson@gmail.com>
2023-11-30 22:26:11 -0600

    safety img

--- src/components/Safety/index.jsx
+++ src/components/Safety/index.jsx
@@ -7,8 +7,8 @@ function Safety () {
     return (
         <div className='safety-card'>
             <h5>Click images to open in new tab</h5>
-        <a href='/src/images/safetycardfront.png' target='_blank' rel="noreferrer"><img src={safetyFront} alt='Click to open in new tab'/></a>
-        <a href='/src/images/safetycardback.png' target='_blank' rel="noreferrer"><img src={safetyBack} alt='Click to open in new tab'/></a>
+        <a href={safetyFront} target='_blank' rel="noreferrer"><img src={safetyFront} alt='Click to open in new tab'/></a>
+        <a href={safetyBack} target='_blank' rel="noreferrer"><img src={safetyBack} alt='Click to open in new tab'/></a>
         <NavLink exact to='/detailed'>BACK</NavLink>
         </div>
     )
