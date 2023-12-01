5af88c06fc2e3f406ec44cb15c4ec1e1d996826c
Ryan <ryanjerickson@gmail.com>
2023-11-30 20:33:35 -0600

    change hl to now

--- src/components/WebcamHelp/index.jsx
+++ src/components/WebcamHelp/index.jsx
@@ -6,7 +6,7 @@ function WebcamHelp() {
     <div className="webcam-help">
       <h3>Steps to fix Webcam:</h3>
       <ol className="help-list">
-        <li><a href='https://webcam.skydivecsc.com/hangar_nw?1701396834238' target="_blank" rel="noreferrer">Click here to open webcam in new tab.</a></li>
+        <li><a href={`https://webcam.skydivecsc.com/hangar_nw?${Date.now()}`} target="_blank" rel="noreferrer">Click here to open webcam in new tab.</a></li>
         <li>Edit webcam address and add https:// before the site. </li>
         <li>If prompted about security click advanced and then proceed.</li>
         <li><NavLink exact to='/webcams'>Navigate back to the app and it should be working!</NavLink></li>
