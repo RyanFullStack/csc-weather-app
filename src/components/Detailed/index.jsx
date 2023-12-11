08fd2ab1c84cb6b6656a07945da9d1bc3e90d78a
Ryan <ryanjerickson@gmail.com>
2023-12-10 21:22:46 -0600

    trivia link

--- src/components/Detailed/index.jsx
+++ src/components/Detailed/index.jsx
@@ -294,6 +294,10 @@ function DetailedPage() {
             <td><NavLink exact to="/safety">Click for safety</NavLink></td>
           </tr>
           <tr className={darkTheme === "true" ? "table" : "table-light"}>
+            <td>Trivia Game:</td>
+            <td><a href="https://csc-trivia.netlify.app/" rel='noreferrer' target='_blank'>Click to play!</a></td>
+          </tr>
+          <tr className={darkTheme === "true" ? "table" : "table-light"}>
             <td>Loading Area View:</td>
             <td>
               <a href="/loadingarea">Click to view</a>
