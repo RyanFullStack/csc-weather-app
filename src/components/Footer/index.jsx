58e5d9ac042e21c08d8737352220b348547bdb87
Ryan <ryanjerickson@gmail.com>
2024-01-05 15:28:27 -0600

    scroll text update

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -27,7 +27,7 @@ function Footer() {
                 ) : (
                     <div id='scroll-container'>
                         <div id='scroll-text'>
-                            <span>HAPPY NEW YEAR FROM CSC!</span><span className='red'>SAFETY DAY 4/6/24</span>
+                            <span>SAFETY DAY 4/6/24</span><span>OPENING DAY 4/19/24</span>
                         </div>
                     </div>
                 )
