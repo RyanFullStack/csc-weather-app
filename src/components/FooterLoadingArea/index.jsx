bda8b8bf85fa8ce0b57326b4c64952b0fb3e1037
Ryan <ryanjerickson@gmail.com>
2024-04-12 10:45:50 -0500

    footer css refactor

--- src/components/FooterLoadingArea/index.jsx
+++ src/components/FooterLoadingArea/index.jsx
@@ -1,5 +1,6 @@
 import { useContext } from "react";
 import { WeatherContext } from "../../context/WeatherContext";
+import './loadingfooter.css'
 
 function FooterLoadingArea() {
   const { jumpruns, maxGust, maxSpeed, speed } = useContext(WeatherContext);
