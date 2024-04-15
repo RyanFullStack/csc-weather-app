4c3432108e0005c58a327c220f99646971748a08
Ryan <ryanjerickson@gmail.com>
2024-04-15 09:36:08 -0500

    rm - from footer message

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -25,7 +25,7 @@ function Footer() {
                 ) : (
                     <div id='scroll-container'>
                         <div id='scroll-text'>
-                            <span>OPENING THIS FRIDAY - 4/19 @ 12pm</span>
+                            <span>OPENING THIS FRIDAY 4/19 @ 12pm</span>
                         </div>
                     </div>
                 )
