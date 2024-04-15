88bef865c83491ba14a1b5d50e8509c4fda067b7
Ryan <ryanjerickson@gmail.com>
2024-04-15 09:35:55 -0500

    footer message

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -25,7 +25,7 @@ function Footer() {
                 ) : (
                     <div id='scroll-container'>
                         <div id='scroll-text'>
-                            <span>OPENING DAY 4/19/24</span>
+                            <span>OPENING THIS FRIDAY - 4/19 @ 12pm</span>
                         </div>
                     </div>
                 )
