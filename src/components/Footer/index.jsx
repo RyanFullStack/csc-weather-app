46231eecf7b8efb21f81a46cafc9030dace36eeb
Ryan <ryanjerickson@gmail.com>
2024-05-03 12:56:20 -0500

    footer jumprun sizing and visual direction

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,38 +1,66 @@
 import { useContext } from "react";
 import { WeatherContext } from "../../context/WeatherContext";
-import './footer.css';
+import "./footer.css";
 
 function Footer() {
-    const { jumpruns, newSpot, newOffset } = useContext(WeatherContext)
+  const { jumpruns, newSpot, newOffset } = useContext(WeatherContext);
 
-    return (
-        <div className="metar-container">
-            <p><a href='https://ryanerickson.netlify.app/' target="_blank" rel='noreferrer'>Created by: Ryan Erickson</a></p>
+  return (
+    <div className="metar-container">
+      <p>
+        <a
+          href="https://ryanerickson.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Created by: Ryan Erickson
+        </a>
+      </p>
 
-            {
-                jumpruns[0] ? (
-                    jumpruns[0].beerLight ? (
-                        <span className="yellow weather-hold">*** BEER LIGHT IS ON! ***</span>
-                    ) : jumpruns[0].weatherHold ? (
-                        <span className="red weather-hold">*** DZ {jumpruns[0].weatherType} HOLD ***</span>
-                    ) : jumpruns[0].heading ? (
-                        <div className="footer-jumprun">
-                            <div><small>Jump Run</small></div>
-                            <div>Direction: {jumpruns[0]?.heading}º</div>
-                            <div>Green Light: {newSpot} {jumpruns[0].selectedSpot} {jumpruns[0].selectedOffset === 'None' ? null : `| ${newOffset} ${jumpruns[0].selectedOffset}`}</div>
-                        </div>
-                    ) : null
-                ) : (
-                    <div id='scroll-container'>
-                        <div id='scroll-text'>
-                            <span>NOW OPEN 5 DAYS!&nbsp;&nbsp;THURS - MON</span>
-                        </div>
-                    </div>
-                )
-            }
-            <p><a href='https://github.com/RyanFullStack/csc-weather-app#Features' target="_blank" rel='noreferrer'>Github</a></p>
+      {jumpruns[0] ? (
+        jumpruns[0].beerLight ? (
+          <span className="yellow weather-hold">*** BEER LIGHT IS ON! ***</span>
+        ) : jumpruns[0].weatherHold ? (
+          <span className="red weather-hold">
+            *** DZ {jumpruns[0].weatherType} HOLD ***
+          </span>
+        ) : jumpruns[0].heading ? (
+          <div className="footer-jumprun">
+            <div>
+              Jumprun: {jumpruns[0]?.heading}º{" "}
+              <i
+                className="fas fa-plane"
+                style={{
+                  transform: `rotate(${jumpruns[0].heading - 90}deg`,
+                }}
+              />
+            </div>
+            <div>
+              Green Light: {newSpot} {jumpruns[0].selectedSpot}{" "}
+              {jumpruns[0].selectedOffset === "None"
+                ? null
+                : `| ${newOffset} ${jumpruns[0].selectedOffset}`}
+            </div>
+          </div>
+        ) : null
+      ) : (
+        <div id="scroll-container">
+          <div id="scroll-text">
+            <span>NOW OPEN 5 DAYS!&nbsp;&nbsp;THURS - MON</span>
+          </div>
         </div>
-    )
+      )}
+      <p>
+        <a
+          href="https://github.com/RyanFullStack/csc-weather-app#Features"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Github
+        </a>
+      </p>
+    </div>
+  );
 }
 
 export default Footer;
