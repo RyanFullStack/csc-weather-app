08925bc33e36f59a6f8637f4063fc5a9aa888879
Ryan <ryanjerickson@gmail.com>
2024-05-05 20:38:16 -0500

    load provider

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import WeatherProvier from "./context/WeatherContext";
-import { BrowserRouter } from "react-router-dom";
+import LoadProvider from "./context/LoadContext.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <WeatherProvier>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <LoadProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </LoadProvider>
     </WeatherProvier>
   </React.StrictMode>
 );
