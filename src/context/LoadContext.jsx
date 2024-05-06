c6a692aca1ccf3b1692e37eec353d40976571269
Ryan <ryanjerickson@gmail.com>
2024-05-05 20:35:10 -0500

    getLoad func

--- src/context/LoadContext.jsx
+++ src/context/LoadContext.jsx
@@ -5,9 +5,18 @@ export const LoadContext = createContext();
 const LoadProvider = (props) => {
     const [loads, setLoads] = useState([])
 
+    const getLoads = async () => {
+        const res = await fetch("https://csc-login.onrender.com/api/loads")
+        if (res.loads) {
+            setLoads(res.loads)
+        } else {
+            setLoads({error: "Can't connect to burble!"})
+        }
+    }
+
 
     return (
-        <LoadContext.Provider value={{}}>
+        <LoadContext.Provider value={{loads}}>
             {props.children}
         </LoadContext.Provider>
     )
