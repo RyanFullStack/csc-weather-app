cb6cc5d01b939816bea2e8e06fd73d28350f8bbf
Ryan <ryanjerickson@gmail.com>
2024-05-07 00:04:54 -0500

    jumper search function and test data submit live for multitest

--- src/components/Manifest/index.jsx
+++ src/components/Manifest/index.jsx
@@ -1,139 +1,173 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { LoadContext } from "../../context/LoadContext";
 import { WeatherContext } from "../../context/WeatherContext";
+import { multiData } from "./testdata";
 import "./manifest.css";
 
 function Manifest() {
-  const { loads } = useContext(LoadContext);
+  let { loads } = useContext(LoadContext);
   const { darkTheme } = useContext(WeatherContext);
+
+  const [searchValue, setSearchValue] = useState("");
 
   if (loads.error) {
     return <div>Can't connect to burble :(</div>;
   }
 
+  loads = multiData;
+
+  const handleSearch = (e) => {
+    setSearchValue(e.target.value);
+  };
+
+  const handleClear = () => {
+    setSearchValue("");
+  };
+
   return (
-    <div className="manifest-content">
-      {loads.every((load) => !load.name) ? (
-        <div
-          className={
-            darkTheme === "true" ? "single-load" : "single-load loadlight"
-          }
-        >
-          No Loads
+    <>
+      {!loads.every((load) => !load.name) ? (
+        <div className="jumper-search">
+          <input
+            onChange={handleSearch}
+            value={searchValue}
+            id="jumper-search-input"
+            placeholder="Search for a name..."
+          ></input>
+          <button id="clear-search" onClick={handleClear}>
+            CLEAR
+          </button>
         </div>
-      ) : (
-        loads.map((load, index) => {
-          if (load.name) {
-            const pattern = /([a-zA-Z\s]+)(\d+)/;
-            const match = load.name.match(pattern);
-            let text, number;
-            if (match) {
-              text = match[1].trim();
-              number = parseInt(match[2]);
-            } else {
-              text = load.name;
-              number = null;
+      ) : null}
+
+      <div className="manifest-content">
+        {loads.every((load) => !load.name) ? (
+          <div
+            className={
+              darkTheme === "true" ? "single-load" : "single-load loadlight"
             }
+          >
+            No Loads
+          </div>
+        ) : (
+          loads.map((load, index) => {
+            if (load.name) {
+              const pattern = /([a-zA-Z\s]+)(\d+)/;
+              const match = load.name.match(pattern);
+              let text, number;
+              if (match) {
+                text = match[1].trim();
+                number = parseInt(match[2]);
+              } else {
+                text = load.name;
+                number = null;
+              }
 
-            const slotsRemaining =
-              load.max_slots - load.public_slots - load.reserve_slots;
-
-            return (
-              <div
-                className={
-                  darkTheme === "true" ? "single-load" : "single-load loadlight"
-                }
-                key={index}
-              >
-                <div className="single-load-header">
-                  <div className="load-header-item first">
-                    <span id="slot-count">
-                      {slotsRemaining}
-                      <span id="small">
-                        {slotsRemaining === 1 || slotsRemaining === -1
-                          ? "Slot"
-                          : "Slots"}
-                      </span>
-                    </span>
-                  </div>
-                  <div className="load-header-item">
-                    {number ? (
-                      <>
-                        {number} <span id="small">{text}</span>
-                      </>
-                    ) : (
-                      text
-                    )}
-                  </div>
-                  <div className="load-header-item end">
-                    {load.status === "On Call" ? (
-                      <span
-                        className={
-                          load.time_left < 6
-                            ? "load-time red"
-                            : load.time_left < 11
-                            ? "load-time yellow"
-                            : "load-time green"
-                        }
-                      >
-                        {load.time_left}{" "}
+              const slotsRemaining =
+                load.max_slots - load.public_slots - load.reserve_slots;
+              return (
+                <div
+                  className={
+                    darkTheme === "true"
+                      ? "single-load"
+                      : "single-load loadlight"
+                  }
+                  key={index}
+                >
+                  <div className="single-load-header">
+                    <div className="load-header-item first">
+                      <span id="slot-count">
+                        {slotsRemaining}
                         <span id="small">
-                          {load.time_left === 1 || load.time_left === -1
-                            ? "Min"
-                            : "Mins"}
+                          {slotsRemaining === 1 || slotsRemaining === -1
+                            ? "Slot"
+                            : "Slots"}
                         </span>
                       </span>
-                    ) : (
-                      <span id="small">{load.status}</span>
-                    )}
+                    </div>
+                    <div className="load-header-item">
+                      {number ? (
+                        <>
+                          {number} <span id="small">{text}</span>
+                        </>
+                      ) : (
+                        text
+                      )}
+                    </div>
+                    <div className="load-header-item end">
+                      {load.status === "On Call" ? (
+                        <span
+                          className={
+                            load.time_left < 6
+                              ? "load-time red"
+                              : load.time_left < 11
+                              ? "load-time yellow"
+                              : "load-time green"
+                          }
+                        >
+                          {load.time_left}{" "}
+                          <span id="small">
+                            {load.time_left === 1 || load.time_left === -1
+                              ? "Min"
+                              : "Mins"}
+                          </span>
+                        </span>
+                      ) : (
+                        <span id="small">{load.status}</span>
+                      )}
+                    </div>
+                  </div>
+                  <div className="single-load-jumpers">
+                    {load.groups.map((group, firstIndex) => {
+                      return group.map((person, index) => {
+                        return person.name
+                          .toLowerCase()
+                          .includes(searchValue.toLowerCase()) ? (
+                          <div
+                            className={
+                              person.type === "Student" && firstIndex % 2 === 0
+                                ? "single-jumper student"
+                                : person.type === "Student" &&
+                                  firstIndex % 2 !== 0
+                                ? "single-jumper student studentodd"
+                                : person.type === "Tandem" &&
+                                  firstIndex % 2 === 0
+                                ? "single-jumper tandem"
+                                : person.type === "Tandem" &&
+                                  firstIndex % 2 !== 0
+                                ? "single-jumper tandem tandemodd"
+                                : darkTheme === "true"
+                                ? "single-jumper"
+                                : "single-jumper jumperlight"
+                            }
+                            key={index}
+                          >
+                            <div id="jumper-name">{person.name}</div>
+                            <div id="jumper-team">
+                              <span id="small">
+                                {person.team_name
+                                  ? person.team_name
+                                  : person.group_number}
+                              </span>
+                            </div>
+                            <div id="jumper-jump">
+                              {person.jump} <br />
+                              <span id="small">
+                                {person.option_name} {person.handycam_jump}
+                              </span>
+                            </div>
+                          </div>
+                        ) : null;
+                      });
+                    })}
                   </div>
                 </div>
-                <div className="single-load-jumpers">
-                  {load.groups.map((group, firstIndex) => {
-                    return group.map((person, index) => {
-                      return (
-                        <div
-                          className={
-                            person.type === "Student" && firstIndex % 2 === 0
-                              ? "single-jumper student"
-                              : person.type === "Student" &&
-                                firstIndex % 2 !== 0
-                              ? "single-jumper student studentodd"
-                              : person.type === "Tandem" && firstIndex % 2 === 0
-                              ? "single-jumper tandem"
-                              : person.type === "Tandem" && firstIndex % 2 !== 0
-                              ? "single-jumper tandem tandemodd"
-                              : darkTheme === "true"
-                              ? "single-jumper"
-                              : "single-jumper jumperlight"
-                          }
-                          key={index}
-                        >
-                          <div id="jumper-name">{person.name}</div>
-                          <div id="jumper-team">
-                            <span id="small">
-                              {person.team_name
-                                ? person.team_name
-                                : person.group_number}
-                            </span>
-                          </div>
-                          <div id="jumper-jump">
-                            {person.jump} <br />
-                            <span id="small">
-                              {person.option_name} {person.handycam_jump}
-                            </span>
-                          </div>
-                        </div>
-                      );
-                    });
-                  })}
-                </div>
-              </div>
-            );
-          }
-        })
-      )}
-    </div>
+              );
+            }
+          })
+        )}
+      </div>
+    </>
   );
 }
 
