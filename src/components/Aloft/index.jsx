3702acf01f956c56f8d85640dade9aaad1177056
Ryan <ryanjerickson@gmail.com>
2024-05-08 20:47:31 -0500

    aloft card bg

--- src/components/Aloft/index.jsx
+++ src/components/Aloft/index.jsx
@@ -26,65 +26,67 @@ function WindsAloft() {
 
   return (
     <div className="wind-aloft-table">
-      <AloftIsValid />
-      <div className="aloft-contents">
-        <table>
-          <thead>
-            <tr>
-              <th>Altitude</th>
-              <th>Direction</th>
-              <th>Speed</th>
-              <th>Temp</th>
-            </tr>
-          </thead>
-          <tbody>
-            {Array.from({ length: 18 }, (_, index) => {
-              const altitude =
-                unitSetting === "true"
-                  ? `${index + 1},000'`
-                  : `${(index + 1) * 304}M`;
-              const altitudeKey = `${(index + 1) * 1000}`;
+      <div className={darkTheme === 'true' ? 'aloft-card' : 'aloft-card aloftlight'}>
+        <AloftIsValid />
+        <div className="aloft-contents">
+          <table>
+            <thead>
+              <tr>
+                <th>Altitude</th>
+                <th>Direction</th>
+                <th>Speed</th>
+                <th>Temp</th>
+              </tr>
+            </thead>
+            <tbody>
+              {Array.from({ length: 18 }, (_, index) => {
+                const altitude =
+                  unitSetting === "true"
+                    ? `${index + 1},000'`
+                    : `${(index + 1) * 304}M`;
+                const altitudeKey = `${(index + 1) * 1000}`;
 
-              return (
-                <tr
-                  className={darkTheme === "true" ? "table" : "table-light"}
-                  key={altitudeKey}
-                >
-                  <td>{altitude}</td>
-                  <td>
-                    {directions[altitudeKey]}º{" "}
-                    <i
-                      className="fas fa-chevron-up"
-                      style={{
-                        transform: `rotate(${
-                          directions[altitudeKey] + 180
-                        }deg)`,
-                      }}
-                    ></i>
-                  </td>
-                  <td>
-                    {speedUnit === "true"
-                      ? `${speeds[altitudeKey]} kts`
-                      : `${Math.round(speeds[altitudeKey] * 1.151)} mph`}
-                  </td>
-                  <td
-                    style={{
-                      color: calculateTemperatureColor(
-                        temps[altitudeKey] * (9 / 5) + 32
-                      ),
-                      fontWeight: "bold",
-                      textShadow: "1px 1px 2px black",
-                    }}
+                return (
+                  <tr
+                    className={darkTheme === "true" ? "table" : "table-light"}
+                    key={altitudeKey}
                   >
-                    {tempSetting === "false"
-                      ? `${temps[altitudeKey]}º C`
-                      : `${Math.round(temps[altitudeKey] * (9 / 5) + 32)}º F`}
-                  </td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
+                    <td>{altitude}</td>
+                    <td>
+                      {directions[altitudeKey]}º{" "}
+                      <i
+                        className="fas fa-chevron-up"
+                        style={{
+                          transform: `rotate(${
+                            directions[altitudeKey] + 180
+                          }deg)`,
+                        }}
+                      ></i>
+                    </td>
+                    <td>
+                      {speedUnit === "true"
+                        ? `${speeds[altitudeKey]} kts`
+                        : `${Math.round(speeds[altitudeKey] * 1.151)} mph`}
+                    </td>
+                    <td
+                      style={{
+                        color: calculateTemperatureColor(
+                          temps[altitudeKey] * (9 / 5) + 32
+                        ),
+                        fontWeight: "bold",
+                        textShadow: "1px 1px 2px black",
+                      }}
+                    >
+                      {tempSetting === "false"
+                        ? `${temps[altitudeKey]}º C`
+                        : `${Math.round(temps[altitudeKey] * (9 / 5) + 32)}º F`}
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
       </div>
     </div>
   );
