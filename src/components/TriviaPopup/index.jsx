56e5fd49059afe45892aa95ad390e087d1bcc01a
Ryan <ryanjerickson@gmail.com>
2024-06-23 19:34:51 -0500

    popup working

--- src/components/TriviaPopup/index.jsx
+++ src/components/TriviaPopup/index.jsx
@@ -1,11 +1,25 @@
+import { useState } from 'react';
 import './trivia.css'
 
 function TriviaPopup() {
+    const [showTrivia, setShowTrivia] = useState(localStorage.getItem('triviaPopup') || 'true')
 
+    const handleTriviaPopup = () => {
+        if (showTrivia === 'true') {
+            setShowTrivia('false')
+            localStorage.setItem('triviaPopup', 'false')
+        } else {
+            setShowTrivia('true')
+            localStorage.setItem('triviaPopup', 'true')
+        }
+    }
 
     return (
-        <div className="trivia-popup">
-            The trivia game blah blah
+        <div className={showTrivia === 'true' ? 'trivia-popup' : 'trivia-popup trivia-hidden'}>
+            <span id='trivia-title'>Save the trivia game!</span> Play at <a href='https://trivia.skydivecsc.com' target='_blank' rel='noreferrer'>trivia.skydivecsc.com</a>
+            or find the link in the 'Detailed' tab.
+            If the top 10 on the leaderboard is not filled by the end of the month the free jump game will be cancelled :(
+            <button onClick={handleTriviaPopup} id='trivia-dismiss'>Dismiss</button>
         </div>
     )
 }
